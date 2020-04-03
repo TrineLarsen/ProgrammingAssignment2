f1293113a12e920e8c7e0bf155547fa3c33e4109
TrineLarsen <63059096+TrineLarsen@users.noreply.github.com>
2020-04-03 12:59:37 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,44 @@
 ## functions do
 
 ## Write a short comment describing this function
+#The makeCacheMatrix function takes a matrix as an argument and creates a list with functions which can be used to cache a variable by another function, in this case cacheSolve.
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
+##Goes to the list created by makeCacheMatrix and assign the getinv function to m. m is a local variable in the cacheSolve environment.
+##Checks if m is null. If m is not null it returns m. This means that the inverse matrix has been calculated previosly.
+##If matrix is null (which it will always be the first time the function is executed) it goes into the list created by makeCacheMatrix function
+## and get the matrix originally passed as an argument to makeCacheSolve and store it as a local variable called data.
+##The function then takes the inverse of the matrix and assign it to m. 
+##The setinv function in makeCacheMatrix is then called which assigns the inverse matrix to m in makeCacheSolve environment.
+##In the end the function return a matrix that is the inverse of 'x'
+
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()                   
+  if(!is.null(m)) {                 
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()                   
+  m <- solve(data, ...)            
+  x$setinv(m)                      
+  m                                 
+  
+  
 }
